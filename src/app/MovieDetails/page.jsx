07c11a6e7f99e8dc7162f79ed06d29f0feb78abd
Naphtali Morage <naphtalimorage56@gmail.com>
2024-07-02 18:32:59 +0300

    creating a metadata and assigning it to movies

---
+++ src/app/MovieDetails/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import useFetch from "@/app/usefetch/useFetchMovies";
+
+export default function movieDetails() {
+  const API_KEY = "9e405034103c33bc18daf866985f6671";
+
+  return (
+    <div>
+        <img src="" alt=""/>
+    </div>
+  );
+   
+}
