94495964a5ce081929fe2d5a0adc7a13951b8fe9
Naphtali Morage <naphtalimorage56@gmail.com>
2024-07-04 13:58:17 +0300

    structur the movie details page nad some styling

--- src/app/MovieDetails/page.jsx
+++ src/app/MovieDetails/page.jsx
@@ -1,13 +1,18 @@
 "use client";
-import useFetch from "@/app/usefetch/useFetchMovies";
+import useFetchMovies from "../fetching/useFetchmovies";
 
-export default function movieDetails() {
+export default function MovieDetails() {
   const API_KEY = "9e405034103c33bc18daf866985f6671";
+  const endpoint = "now_playing";
 
+  const {data: movie} = useFetchMovies(endpoint, API_KEY);
   return (
     <div>
-        <img src="" alt=""/>
+        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}/>
+        <h2>{movie.title}</h2>
+        <h2>{new Date(movie.release_date).getFullYear()}</h2>
+        <p>{movie.overview}</p>
     </div>
   );
    
-}
+};
