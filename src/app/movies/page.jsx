924e547dd175a1742a0d2eb790847505a0bbb111
Naphtali Morage <naphtalimorage56@gmail.com>
2024-07-04 14:03:20 +0300

    structuring it so as to use movieRow component and fetch different category of movies

--- src/app/movies/page.jsx
+++ src/app/movies/page.jsx
@@ -1,27 +1,36 @@
 "use client";
-import useFetchMovies from "../usefetch/useFetchMovies";
-import Image from "next/image";
-import Film from "../film/page";
-
+import useFetchMovies from "../fetching/useFetchmovies";
+import MovieRow from "../movielist/movielist";
 export default function Movies() {
   const API_KEY = "9e405034103c33bc18daf866985f6671";
 
-  const endpoint = "now_playing";
-  const { data: nowplayings} = useFetchMovies(endpoint, API_KEY);
+  const endpoint1 = "now_playing";
+  const endpoint2 = "popular";
+  const endpoint3 = "top_rated";
+  const endpoint4 = "upcoming";
 
- 
+  const { data: nowPlayingMovies } = useFetchMovies(endpoint1, API_KEY);
+  const { data: popularMovies } = useFetchMovies(endpoint2, API_KEY);
+  const { data: topRatedMovies } = useFetchMovies(endpoint3, API_KEY);
+  const { data: upcomingMovies } = useFetchMovies(endpoint4, API_KEY);
 
+  const nowPlayingCategory = "Now Playing";
+  const PopularCategory = "Popular";
+  const TopRatedCategory = "Top Rated";
+  const UpcomingCategory = "Upcoming";
   return (
-    <div className=" ml-5 mt-4 px-5">
-    <h2 className="text-2xl ">Now Playing</h2>
-    <div className="flex  overflow-x-auto scroll-snap-type-x-mandatory scroll-touch mt-2">
-        {nowplayings && nowplayings.map((movie) => {
-          return (
-            <div key={movie.id}>
-              <Film  filmTitle={movie.title} releaseDate={movie.release_date} posterUrl={movie.poster_path}/>
-            </div>
-          )
-        })}
+    <div>
+      <div className=" ml-5 mt-4 px-5">
+        <MovieRow category={nowPlayingCategory} movies={nowPlayingMovies} />
+      </div>
+      <div className=" ml-5 mt-4 px-5">
+        <MovieRow category={PopularCategory} movies={popularMovies} />
+      </div>
+      <div className=" ml-5 mt-4 px-5">
+        <MovieRow category={TopRatedCategory} movies={topRatedMovies} />
+      </div>
+      <div className=" ml-5 mt-4 px-5">
+        <MovieRow category={UpcomingCategory} movies={upcomingMovies} />
       </div>
     </div>
   );
