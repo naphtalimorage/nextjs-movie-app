de179dcdbc890d5a32b8af48e3f2400fee6db1a7
Naphtali Morage <naphtalimorage56@gmail.com>
2024-07-04 14:12:39 +0300

    handle loading when fetching data

--- src/app/usefetch/loading.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-red-800"></div>
-    </div>
-  );
-}
