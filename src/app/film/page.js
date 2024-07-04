86eac5447fc7ddb3c4386660540a82d26e6184f8
Naphtali Morage <naphtalimorage56@gmail.com>
2024-07-04 13:59:52 +0300

    making it dynamic and passing passing props

--- src/app/film/page.js
+++ src/app/film/page.js
@@ -1,14 +1,20 @@
 "use client"
+
+import Link from "next/link";
+
 export default function Film({ filmTitle, posterUrl, releaseDate}) {
   return (
-    <div>
+    <div className="w-40 h-70 ">
+      <Link href="/moviedetails">
       <img
         src={`https://image.tmdb.org/t/p/w500${posterUrl}`}
         alt={filmTitle}
-        width={75}
-        height={75}
-      className="border-r-7"/>
-      <h3>{filmTitle}</h3>
+        width={150}
+        height={80}
+      className="rounded-lg"/>
+      </Link>
+      
+      <h3 className="whitespace-nowrap overflow-hidden text-ellipsis text-lg font-semibold">{filmTitle}</h3>
       <h3>{new Date(releaseDate).getFullYear()}</h3>
     </div>
   );
